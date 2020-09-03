69919ed6d3a201d1421c2828f7a8836626ae460b
Simoncao <13671707129@126.com>
2020-09-03 14:05:08 +0800

    [feat]add lastChangeTime

--- OPSAdmin/gameOperation/views.py
+++ OPSAdmin/gameOperation/views.py
@@ -7,7 +7,9 @@ from django.views.decorators.csrf import csrf_exempt
 
 @login_required
 def changeNotice(request):
-    return render(request, 'gameOperation/changeNotice.html')
+    monitorFile = '/home/langrisser-list/conf/0204qa_notice.txt'
+    lastChangeTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.path.getmtime(monitorFile)))
+    return render(request, 'gameOperation/changeNotice.html',context={'lastChangeTime': lastChangeTime})
 
 @login_required
 @csrf_exempt
