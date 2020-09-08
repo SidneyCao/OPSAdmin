f168b7bc3700314d02279f2eaa1f30bcf6987c96
Simoncao <13671707129@126.com>
2020-09-08 18:31:36 +0800

    [feat]changeTime Restore stop

--- OPSAdmin/gameOperation/views.py
+++ OPSAdmin/gameOperation/views.py
@@ -92,7 +92,7 @@ def changeTimeExecStop(request):
             os.popen('/home/langrisser-shell-scripts/Server/%s/ts_stopserver.sh' %execType)
             time.sleep(5)
             res = os.popen('/home/langrisser-shell-scripts/Server/%s/check_process.sh' %execType).read()
-            if('%s Process Start Success' %execType in res):
+            if(res == '%s \033[33mProcess Stop Success\033[0m' %execType):
                 return JsonResponse({"process":"stop", "status":"success"})
             else:
                 return JsonResponse({"process":"stop", "status":"fail"})
