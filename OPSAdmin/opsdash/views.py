fca861bde56eb433491589e4f252d77197a0b1e3
Simoncao <13671707129@126.com>
2020-09-14 16:00:39 +0800

    [feat]新增操作日志 登陆成功打点

--- OPSAdmin/opsdash/views.py
+++ OPSAdmin/opsdash/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+import datetime
 
 operationLogFile = settings.OPER_LOG_FILE
 IsoTimeFormat='%Y-%m-%d %H:%M:%S'
